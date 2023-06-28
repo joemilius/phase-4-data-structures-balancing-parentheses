da08868cf4c3671ae116a557437f6735325b819a
Joe Milius <joemilius@gmail.com>
2023-06-27 22:52:05 -0600

    stack parentheses

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,20 @@
 require_relative './stack'
 
 # your code here
+def balancing_parentheses(paras)
+    stack = Stack.new
+    
+    added = 0
+    
+
+    paras.each_char do |char|
+        if stack.peek == '(' && char == ')'
+            stack.pop
+        else
+            stack.push(char)
+        end
+    end
+
+    stack.size
+
+end
